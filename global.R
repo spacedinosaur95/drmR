c36c78d84eae79a358c85c0525073fe6538c9ad8
spacedinosaur95 <rebeccalorraine.demarco@gmail.com>
2016-10-29 20:08:11 -0400

    Create global.R

---
+++ global.R
@@ -0,0 +1,14 @@
+## Load packages
+## Do Not Delete. Needed for dashboard ##
+library(shiny)
+library(shinydashboard)
+library(shinyapps)
+#####################################
+## Add your specific pacakages
+library(rmarkdown)
+library(markdown)
+library(gplots) ## for heatmap.2
+library(RColorBrewer)
+library(tm)
+library(wordcloud)
+library(qdap)
